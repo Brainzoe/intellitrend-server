61b08f6abebf15b0412daa0563cb3b9d2ca6fe92
Brainzoe <johnigolo@gmail.com>
2025-08-23 23:42:43 +0100

    authController fix

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -18,14 +18,18 @@ if (!process.env.SMTP_HOST || !process.env.SMTP_PORT || !process.env.SMTP_USER |
 }
 
 const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: Number(process.env.SMTP_PORT),
-  secure: Number(process.env.SMTP_PORT) === 465, // true for 465
+  host: process.env.EMAIL_HOST,
+  port: Number(process.env.EMAIL_PORT),
+  secure: false, // TLS over 587
   auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false, // allow self-signed certs
   },
 });
+
 
 // Utility function to send email
 const sendEmail = async (to: string, subject: string, html: string) => {
